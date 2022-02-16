e4a3cd7cf04390db26042bbda66fcf9fa2e1fe15
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-16 14:14:22 +0530

    Add project files.

---
+++ final/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class MyClient
+{
+    public static void Main()
+    {
+        int x = 0;
+        int div = 0;
+        try
+        {
+            div = 100 / x;
+            Console.WriteLine("Not executed line");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Exception occured");
+        }
+        finally
+        {
+            Console.WriteLine("Finally Block");
+        }
+        Console.WriteLine($"Result is {div}");
+    }
+}
